43c7889dada461643b612f4945e5d453c1c1540d
vanbumi <jsp.dio@gmail.com>
2016-06-13 10:33:57 +0700

    update to github

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,20 @@
 class Cart < ActiveRecord::Base
+	has_many :line_items, dependent: :destroy
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+
+
+	private
+
+	# to ensure that there are no line items referencing this product
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Product sedang di referensikan oleh Line Item')
+			return false
+		end
+	end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :name, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :image_url, allow_blank: true, format: {
